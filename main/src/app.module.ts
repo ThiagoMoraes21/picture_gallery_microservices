da3f463a027029b25b4593961f4603eb86e38aa9
Thiago Moraes <thiago4x70@gmail.com>
2021-07-25 15:09:04 -0300

    add db connection

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.vqq92.mongodb.net/test`
+        ),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
