46e3dd712076ee101413b907bdb3cf948aaa0742
Thiago Moraes <thiago4x70@gmail.com>
2021-07-24 22:26:28 -0300

    add a remote connection to mysql cloud database

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.HOST,
+            port: Number(process.env.PORT),
+            username: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            autoLoadEntities: true, 
+            synchronize: true,
+        }),
+        ProductModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
