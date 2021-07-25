9ce5ecb7852e572b0bd286843fa0860f92d67c75
Thiago Moraes <thiago4x70@gmail.com>
2021-07-25 03:04:13 -0300

    add product entity

--- admin/src/product/product.module.ts
+++ admin/src/product/product.module.ts
@@ -1,7 +1,12 @@
+import { Product } from './entities/product.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Product])
+    ],
     controllers: [ProductController]
 })
 export class ProductModule { }
