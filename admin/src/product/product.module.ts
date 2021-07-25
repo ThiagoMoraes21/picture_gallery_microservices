5cfc18ca45fd30f87ad7b649887062c796fdee61
Thiago Moraes <thiago4x70@gmail.com>
2021-07-25 20:32:13 -0300

    configure microservice product client

--- admin/src/product/product.module.ts
+++ admin/src/product/product.module.ts
@@ -3,10 +3,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Product])
+        TypeOrmModule.forFeature([Product]),
+        ClientsModule.register([
+            {
+                name: 'PRODUCT_SERVICE',
+                transport: Transport.RMQ,
+                options: {
+                    urls: [process.env.AMPQ_URL],
+                    queue: 'main_queue',
+                    queueOptions: {
+                        durable: false
+                    },
+                },
+            },
+        ]),
     ],
     controllers: [ProductController],
     providers: [ProductService]
